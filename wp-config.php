450e8c687996ac487e195824a5a84cef1ac95acc
FabianoFaria <fabiano_hatori@yahoo.com.br>
2023-08-15 11:22:17 -0300

    primeiro commit

--- wp-config.php
+++ wp-config.php
@@ -41,13 +41,13 @@ if (!function_exists('getenv_docker')) {
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', '');
 
 /** Database username */
-define( 'DB_USER', 'baraobarao2');
+define( 'DB_USER', '');
 
 /** Database password */
-define( 'DB_PASSWORD', 'segueaSenhasenhuda2#');
+define( 'DB_PASSWORD', '');
 
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
@@ -56,7 +56,7 @@ define( 'DB_PASSWORD', 'segueaSenhasenhuda2#');
  */
 
 /** Database hostname */
-define( 'DB_HOST', '172.26.63.185:3306');
+define( 'DB_HOST', '');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
